867c758b5cc4bc58c6c6ebc177194730ab3092e6
Moshood Olawale <shabimoshood07@gmail.com>
2023-10-05 22:36:58 +0100

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const toggleNavClass = "right-[-500px]";
+  const toggleNavFunt = () => setShow(!show);
+
   return (
     <header className="h-[80px] fixed flex items-center top-0 w-screen">
       <nav className="flex justify-between items-center w-[95%] max-w-[1450px] mx-auto">
+        {/* Logo */}
         <Link href="/">
           <Image
             src="https://uploads-ssl.webflow.com/5e80894f63c557e083ed96b4/5e808ce7dc544553a7f1b1e4_Black.svg"
@@ -13,8 +20,14 @@ const Navbar = () => {
             alt="fitnesso"
           />
         </Link>
-        <div className="border border-red-500 flex">
-          <ul className=" justify-end items-center  hidden lg:flex">
+
+        <div className="flex">
+          {/* NAV LINKS */}
+          <ul
+            className={`items-center flex-col flex fixed -z-10 top-0 h-screen w-screen  border px-[50px] pt-[100px] pb-[50px] bg-[#cfddee] duration-500 ease-in-out ${
+              !show ? toggleNavClass : "right-0"
+            } `}
+          >
             <li className="nav-links">
               <Link href="#">About</Link>
             </li>
@@ -24,36 +37,36 @@ const Navbar = () => {
             <li className="nav-links">
               <Link href="#">Shop</Link>
             </li>
-            <li className="nav-links">
-              <Link href="#">
-                <button className="py-[12px] pl-[20px] pr-[20px] bg-[#081158] font-[500] min-w-[160px] uppercase rounded-[6px] font-['Sato'] text-white tracking-[2px] flex items-center justify-start hover:duration-500 hover:pl-[40px] hover:-translate-y-[2px] hover:ease-out hover:opacity-80">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                    />
-                  </svg>
-                  book a call
-                </button>
-              </Link>
-            </li>
+
+            <button className=" w-full py-[12px] pl-[20px] pr-[20px] bg-[#081158] font-[500] min-w-[160px] uppercase rounded-[6px] font-['Sato'] text-white tracking-[2px]  items-center justify-start hover:duration-500 hover:pl-[40px] hover:-translate-y-[2px] ease-out hover:opacity-80 text-center relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 absolute left-0"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+              book a call
+            </button>
           </ul>
           <div className="flex justify-between items-center">
-            <button className="p-[10px] lg:hidden">
+            <button
+              className={`${show?"bg-[#081158]":""} p-[10px] lg:hidden `}
+              onClick={toggleNavFunt}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-5 h-5 text-[#081158] font-extrabold"
-              >
+                className={`${show? "text-white":"text-[#081158]"} w-5 h-5 font-extrabold`} 
+                >
                 <path
                   fillRule="evenodd"
                   d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
